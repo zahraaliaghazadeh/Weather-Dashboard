021ef158d67b7655acaa392c6075f1bc4903d0dd
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-03-24 23:29:22 -0700

    initial file

---
+++ script.js
@@ -0,0 +1,42 @@
+var APIKey= "94ad115827be5f934ef5a524648ee25a";
+
+ 
+
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+      "q=Bujumbura,Burundi&appid=" + APIKey;
+
+
+
+
+$.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+/* We store all of the retrieved data inside of an object called "response" */
+      .then(function(response) {
+
+        // Log the queryURL
+        console.log(queryURL);
+
+        // Log the resulting object
+        console.log(response);
+
+        // Transfer content to HTML
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        
+        // Convert the temp to fahrenheit
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        // add temp content to html
+        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + tempF);
+      });
+
